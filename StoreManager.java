0c373ebc2ef1c4de7264d4579930420bcd650c5e
Raphael Jr <jeasonraphael11@gmail.com>
2025-03-18 15:22:03 +0800

    abab

---
+++ StoreManager.java
@@ -0,0 +1,29 @@
+public class StoreManager {
+    private String name;
+    private Store store;
+
+    public StoreManager(String name) {
+        this.name = name;
+        store = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void createStore(String store_name, String store_address, String contactNumber) {
+        store = new Store(store_name, store_address, contactNumber, this);
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void removeStore() {
+        store = null;
+    }
+}
